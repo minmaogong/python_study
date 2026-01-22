528a6d21f879a13cdb35ac3d81ff52dfe8aa232e
maomin <1220826363@qq.com>
2026-01-23 00:21:02 +0800

    new: 模块打包

---
+++ day09/build/lib/graphic/circle.py
@@ -0,0 +1,8 @@
+radius = 10
+PI = 3.14
+
+def area(radius):
+    return PI * radius * radius
+
+def perimeter(radius):
+    return 2 * PI * radius
---
+++ day09/build/lib/graphic/rectangle.py
@@ -0,0 +1,8 @@
+rectangle_width = 10
+rectangle_height = 10
+
+def area(width, height):
+    return width * height
+
+def perimeter(width, height):
+    return 2 * (width + height)
